407ebcef8f3df1a784e4f43f9745064c0e57ce4f
roberis <python25595@gmail.com>
2025-12-27 02:15:06 -0600

    my work

--- contracts/AutoStake.sol
+++ contracts/AutoStake.sol
@@ -10,6 +10,7 @@ contract AutoStake is Controllable {
 
   NoMintRewardPool public rewardPool;
   IERC20 public lpToken;
+  IERC20 public lpToken1;
   uint256 public unit = 1e18;
   uint256 public valuePerShare = unit;
   uint256 public totalShares = 0;
